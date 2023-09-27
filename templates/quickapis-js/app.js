80e7aee4d00d8ccf0de947430b57cfc32eb9b871
flutterde <chouatiotman2000@gmail.com>
2023-09-27 16:45:17 +0100

    dev init

--- templates/quickapis-js/app.js
+++ templates/quickapis-js/app.js
@@ -1,1 +1,12 @@
-console.log('Hello from EasyApiJs!');
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World 2023!');
+});
+
+
+app.listen(3000, () => {
+    console.log('app listening on port 3000!');
+});
